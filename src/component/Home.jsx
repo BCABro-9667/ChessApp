ee3b740e806bf7a059e19849a5bcc62d88b0812a
Avdhesh_Kumar <avdeshrajput925064@gmail.com>
2024-09-06 21:46:50 +0530

    add why chess

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  './stylesheet/global.css'
+import './stylesheet/global.css'
 import IndianGMOhotos from './other_comp/IndianGM'
 import TournmentBanner from './other_comp/TournmentBanner'
 import chess from './img/chess.png'
@@ -9,24 +9,24 @@ import ChessThinking from './ChessThinking'
 function Home() {
   return (
     <>
-    <div className="chess-play">
-      
-      <div className="img"><img src={chess} alt="chess" /></div>
-      <div className="detailss">
-        <h1>Play Chess Online on the #1 Site!
-          <br /><span>
-            {/* <DateTime/>/ */}
-          </span>
-        </h1>
+      <div className="chess-play">
 
-          
+        <div className="img"><img src={chess} alt="chess" /></div>
+        <div className="detailss">
+          <h1>Play Chess Online on the #1 Site!
+            <br /><span>
+              {/* <DateTime/>/ */}
+            </span>
+          </h1>
+
+
           <a href="https://chess.com/" target='_blank'><button  >Play Online</button></a>
+        </div>
       </div>
-    </div>
-    <ChessThinking/>
+      <ChessThinking />
 
-    <TournmentBanner/>
-    <IndianGMOhotos/>
+      <TournmentBanner />
+      <IndianGMOhotos />
     </>
   )
 }
